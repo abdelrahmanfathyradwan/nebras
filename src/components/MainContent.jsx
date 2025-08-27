dde3c83434a81a58a7242acf164ae5f7fa485228
Abdelrahman2005Fathy <your-email@example.com>
2025-08-27 11:28:07 +0200

    edit size of image

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -50,7 +50,7 @@ const MainContent = () => {
                   <img 
                     src="/images/muslim.jpg" 
                     alt="Quran and Arabic Learning"
-                    className="rounded-lg shadow-lg w-full h-[400px] object-cover object-bottom"/>
+                    className="h-[210px] md:h-[400px] rounded-lg shadow-lg w-full object-cover object-bottom"/>
                 </div>
                 <div className="md:w-1/2 md:pl-8 order-1 md:order-2">
                   <div className='flex gap-5'> 
